3511c0b1ee9e13ca3b04c8042f28b363aef94294
dyshaaa <dyshakrut@gmail.com>
2025-07-13 15:49:34 +0300

    clear html + css

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Happy developing ✨')
+document.addEventListener("DOMContentLoaded", function () {
+    const text = "I'm a Software Engineering Student.";
+    const target = document.querySelector(".hero-about-title");
+    let index = 0;
+
+    function typeWriter() {
+        if (index < text.length) {
+            target.textContent += text.charAt(index);
+            index++;
+            setTimeout(typeWriter, 100);
+        }
+    }
+
+    window.addEventListener("load", typeWriter);
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+
+    document.querySelectorAll('.example-item, .work-card').forEach(el => {
+        observer.observe(el);
+    });
+})
+
